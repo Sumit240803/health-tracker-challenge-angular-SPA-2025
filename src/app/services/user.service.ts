8fe09c4eb6575debfaa352be265dd39d9bb0b077
Sumit240803 <goyalsumit651@gmail.com>
2025-01-29 13:40:29 +0530

    Services Created

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Workout } from '../models/workout.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  private user :User | null = null;
+  private readonly storageKey = 'userData';
+  constructor() { 
+    const savedData = localStorage.getItem(this.storageKey);
+    if(savedData){
+      this.user = JSON.parse(savedData);
+    }
+  }
+  private savetoLocalStorage():void{
+    localStorage.setItem(this.storageKey,JSON.stringify(this.user));
+  }
+  setUsername(username : string):void{
+    if(!this.user){
+      this.user = {username , workouts :[]};
+    }else{
+      this.user.username = username;
+    }
+    this.savetoLocalStorage();
+  }
+  addWorkouts(workout:Omit<Workout,'date'>):void{
+    if(this.user){
+      this.user.workouts.push({...workout,date : new Date()});
+      this.savetoLocalStorage();
+    }
+  }
+  getUser():User |null{
+    return this.user;
+  }
 }
